4a67fba372c929f66e8c49fea9586e313b40edeb
Villy-P <valeriuspetrini@gmail.com>
2022-12-26 16:07:42 -0600

    Make Computers

--- main.lisp
+++ main.lisp
@@ -1,11 +1,20 @@
 (defclass Player ()
     ((name :accessor player-name)))
 
-(defvar mainPlayer (make-instance 'Player))
+(defvar main-player (make-instance 'Player))
+(defvar computer-num 0)
+(defvar computers (make-array '(8)))
 
 (defun main ()
-    (write "What is your name?")
-    (setf (player-name mainPlayer) (read))
-    (format t "Player Name is ~s~%" (player-name mainPlayer)))
-
+    (princ "Welcome to Texas Hold'em!")(terpri)
+    (princ "What is your name? ")
+    (finish-output)(setf (player-name main-player) (read))
+    (loop
+        (princ "How many people do you want to play against? (limit 1 max 8) ")
+        (finish-output)(setf computer-num (read))
+        (when (and (> computer-num 0) (< computer-num 9)) (return computer-num)))
+    (dotimes (i computer-num)
+        (setf (aref computers i) (make-instance 'Player))
+        (setf (player-name (aref computers i)) (format nil "Computer #~d~%" i)))
+        
 (main)
